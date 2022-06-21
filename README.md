4d7426f1004dec44b60e818df7abee3ca8f35581
wengzhisong <1322457349@qq.com>
2022-06-21 14:42:13 +0800

    add: readme

---
+++ README.md
@@ -0,0 +1,36 @@
+参考候捷《STL源码剖析》
+
+进度：
+
+- [ ] iterator
+    - [ ] traits技法
+- [ ] allocator
+    - [ ] 第一级配空间置器
+    - [ ] 第二级空间配置器
+- [ ] 序列式容器
+    - [ ] vector
+    - [ ] list
+    - [ ] deque
+    - [ ] stack
+    - [ ] heap
+    - [ ] priority_quene
+    - [ ] slist
+- [ ] 关联式容器
+    - [ ] RB-tree
+    - [ ] set
+    - [ ] map
+    - [ ] multiset
+    - [ ] multimap
+    - [ ] hashtable
+    - [ ] hash_set
+    - [ ] hash_map
+    - [ ] hash_multiset
+    - [ ] hash_multimap
+- [ ] 算法
+    - [ ] 数值算法
+    - [ ] 基本算法
+    - [ ] set相关算法
+    - [ ] heap算法
+    - [ ] 其它算法
+- [ ] functors
+- [ ] adapters
--- src/type_traits.h
+++ src/type_traits.h
@@ -36,18 +36,6 @@ namespace MicroSTL {
      */
     template<typename Category, typename T, typename Distance = ptrdiff_t, typename Pointer = T *, typename Reference = T &>
     struct iterator {
-        using iterator_category = Category;
-        using value_type = T;
-        using difference_type = Distance;
-        using pointer = Pointer;
-        using reference = Reference;
-    };
-
-    /**
-     * traits
-     */
-    template<class T>
-    struct iterator_traits {
         /**
          * 迭代器类别，分为五类：
          * - input iterator：只读
@@ -56,24 +44,36 @@ namespace MicroSTL {
          * - bidirectional iterator：双向访问，支持读写
          * - random access iterator：随机访问，支持读写
          */
-        using iterator_category = typename T::iterator_category;
+        using iterator_category = Category;
         /**
          * 迭代器所指对象类别；
          */
-        using value_type = typename T::value_type;
+        using value_type = T;
         /**
          * 两个迭代器之间的距离；
          * 可以用来表示容器的最大容量；
          */
-        using difference_type = typename T::difference_type;
+        using difference_type = Distance;
         /**
          * 指向迭代器所指之物的指针；
          */
-        using pointer = typename T::pointer;
+        using pointer = Pointer;
         /**
          * 迭代器所指之物是否允许改变；
          * 对迭代器解引用的时候，返回的是左值还是右值；
          */
+        using reference = Reference;
+    };
+
+    /**
+     * traits
+     */
+    template<class T>
+    struct iterator_traits {
+        using iterator_category = typename T::iterator_category;
+        using value_type = typename T::value_type;
+        using difference_type = typename T::difference_type;
+        using pointer = typename T::pointer;
         using reference = typename T::reference;
     };
 
