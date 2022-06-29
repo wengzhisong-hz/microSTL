b017b96b29da4fcaf32e152c3f49c8aa0091ef3d
wengzhisong <1322457349@qq.com>
2022-06-29 14:13:22 +0800

    test: type traits

--- test/test_type_traits.cpp
+++ test/test_type_traits.cpp
@@ -1,12 +1,50 @@
 #include <gtest/gtest.h>
 #include "../iterator/type_traits.h"
-#include "../iterator/iterator.h"
 
 using namespace MicroSTL;
 
+// 未定义的类型
+template<typename T>
+int get_type(T type) {
+    return 0;
+}
 
-TEST(示例, 测试相等) {
-    EXPECT_EQ(1, 1);
+// 全特化 true_type
+template<>
+int get_type<true_type>(true_type type) {
+    return 1;
+}
+
+// 全特化 false_type
+template<>
+int get_type<false_type>(false_type type) {
+    return 2;
+}
+
+
+TEST(测试type_traits, 测试POD类型) {
+    using t1 = typename type_traits<int>::is_POD_type;
+    using t2 = typename type_traits<char>::is_POD_type;
+    using t3 = typename type_traits<int *>::is_POD_type;
+    using t4 = typename type_traits<const int *>::is_POD_type;
+
+    struct other {
+        int a;
+        char b;
+    };
+
+    t1 o1;
+    t2 o2;
+    t3 o3;
+    t4 o4;
+
+    other o5;
+
+    EXPECT_EQ(1, get_type(o1));
+    EXPECT_EQ(1, get_type(o2));
+    EXPECT_EQ(1, get_type(o3));
+    EXPECT_EQ(1, get_type(o4));
+    EXPECT_EQ(0, get_type(o5));
 }
 
 int main(int argc, char *argv[]) {
