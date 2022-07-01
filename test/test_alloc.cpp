c903ac92cc8b69572c0e9cb4537d59219d8918b8
wengzhisong <1322457349@qq.com>
2022-07-01 13:51:18 +0800

    test: AllocByMalloc

--- test/test_alloc.cpp
+++ test/test_alloc.cpp
@@ -1,11 +1,47 @@
 #include <gtest/gtest.h>
+#include <string>
 #include "../memory/alloc.h"
-
 
 using namespace MicroSTL;
 
-TEST(示例, 测试相等) {
-    EXPECT_EQ(1, 1);
+TEST(AllocByMalloc, allocate) {
+    size_t size = 100;
+    void *ptr = AllocByMalloc::allocate(size);
+    EXPECT_TRUE(ptr);
+}
+
+TEST(AllocByMalloc, reallocate) {
+    size_t size = 10;
+    void *old_ptr = AllocByMalloc::allocate(size);
+    void *new_ptr = AllocByMalloc::reallocate(old_ptr, 10, 20);
+    EXPECT_TRUE(new_ptr);
+}
+
+TEST(AllocByMalloc, deallocate) {
+    size_t size = 10;
+    char *ptr = static_cast<char *>(AllocByMalloc::allocate(size));
+    strcpy(ptr, "12345");
+    char first = *ptr;
+    EXPECT_EQ(first, '1');
+    AllocByMalloc::deallocate(ptr, size);
+    char _first = *ptr;
+    EXPECT_NE(_first, '1');
+}
+
+int temp = 0;
+
+void do_nothing() {
+    temp++;
+};
+
+TEST(AllocByMalloc, set_oom_user_handler) {
+    void (*oom_fn)();
+    void (*old_fn)();
+    oom_fn = do_nothing;
+    old_fn = AllocByMalloc::set_oom_user_handler(oom_fn);
+    old_fn = AllocByMalloc::set_oom_user_handler(oom_fn);
+    oom_fn();
+    EXPECT_EQ(temp, 1);
 }
 
 int main(int argc, char *argv[]) {
